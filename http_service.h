a7da6d43660c327a8bece4bf3ad5e960e5f35837
astercasc <astercass@qq.com>
2023-07-10 15:20:11 +0800

    feat: support module update

---
+++ http_service.h
@@ -0,0 +1,39 @@
+
+#ifndef HTTPSERVICE_H
+#define HTTPSERVICE_H
+
+#include <QByteArray>
+#include <QDebug>
+#include <QEventLoop>
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QObject>
+#include <QRegularExpression>
+#include <QUrl>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
+
+struct FrpResult
+{
+    QString tag;
+    QString latestUrl;
+    QString name;
+};
+
+class HttpService : public QObject
+{
+    Q_OBJECT
+public:
+    explicit HttpService(QObject* parent = nullptr);
+    ~HttpService() override;
+
+public:
+    static bool      downloadFile(QString localPath, FrpResult frpResult, void (*callback)(FrpResult res));
+    static FrpResult getLatestReleaseData(QString owner, QString repo);
+
+signals:
+};
+
+#endif  // HTTPSERVICE_H
