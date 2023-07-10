cd893543ef4d00b447304405f75e4bf650d78bfb
astercasc <astercass@qq.com>
2023-07-10 13:55:51 +0800

    fix: print new log cover old after highlighted old log

--- output_widget.cpp
+++ output_widget.cpp
@@ -12,6 +12,7 @@ OutputWidget::OutputWidget(QWidget* parent) : QWidget{ parent }
         outputView->setObjectName("outputView");
         outputView->setStyleSheet("#outputView{background-color: rgba(25, 25, 25, 100%);font-size:15px;"
                                   "border: 2px solid grey;font-family: 'SimSun';}");
+        outputView->setReadOnly(true);
     }
     // layout
     indexLayout = new QVBoxLayout(this);
@@ -34,18 +35,21 @@ void OutputWidget::clearConsole()
 
 void OutputWidget::printMessageWarn(QString str)
 {
+    outputView->moveCursor(QTextCursor::End);
     outputView->setTextColor(QColor(255, 255, 100));
     outputView->insertPlainText(str.append("\n\n"));
 }
 
 void OutputWidget::printMessageError(QString str)
 {
+    outputView->moveCursor(QTextCursor::End);
     outputView->setTextColor(QColor(255, 100, 100));
     outputView->insertPlainText(str.append("\n\n"));
 }
 
 void OutputWidget::printMessage(QString str)
 {
+    outputView->moveCursor(QTextCursor::End);
     outputView->setTextColor(QColor(100, 255, 100));
     outputView->insertPlainText(str.append("\n"));
     //    printf(str.toUtf8().data());
